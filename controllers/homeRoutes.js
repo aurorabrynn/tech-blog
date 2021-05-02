46cafaba950ad152222212c7a63afb44679ca157
Aurora Duncan <aurorabrynn@gmail.com>
2021-05-02 13:43:07 -0700

    added dashboard and connected comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,32 @@ router.get('/', async (req, res) => {
         const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
         // Pass serialized data and session flag into template
-        res.render('homepage', {
+        res.render('home', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', async (req, res) => {
+    try {
+        // Get all blogs and JOIN with user data
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('dashboard', {
             blogs,
             logged_in: req.session.logged_in
         });
@@ -77,6 +102,16 @@ router.get('/login', (req, res) => {
     }
 
     res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
